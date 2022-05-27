dae902d9104747c64aa3f7050b37a7368e0b4dc3
Luiz Otávio <luizotaviomtbs@gmail.com>
2022-05-27 11:37:37 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o primeiro numero: ");
+            string firstNum = Console.ReadLine();
+            Console.WriteLine("Digite o operador aritmético: ");
+            string op = Console.ReadLine();
+            Console.WriteLine("Digite o segundo numero: ");
+            string secondNum = Console.ReadLine();
+
+            Console.WriteLine(Calculadora(firstNum, secondNum, op));
+        }
+        public static double Calculadora(string x, string y, string n)
+        {
+            double resultado = 0;
+            double num1 = Convert.ToDouble(x);
+            char op = Convert.ToChar(n);
+            double num2 = Convert.ToDouble(y);
+            switch (op)
+            {
+                case '+':
+                    return resultado = num1 + num2;
+                case '-':
+                    return resultado = num1 - num2;
+                case '*':
+                    return resultado = num1 * num2;
+                case '/':
+                    return resultado = num1 / num2;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto inicial - Calculadora em C#
+> Um simples projeto de uma calculadora, lógica em C# ainda sem implementação de um menu para gerenciar/reutilizar. - (.NET 5.0)
